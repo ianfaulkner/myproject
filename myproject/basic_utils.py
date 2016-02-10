d4cb6bf48b9d46b979ddea01c31e794ddea29af0
Ian Faulkner <idf@uw.edu>
2016-02-10 13:32:21 -0800

    Documented basic_utils.py

--- myproject/basic_utils.py
+++ myproject/basic_utils.py
@@ -1,4 +1,16 @@
 from .constants import pi
 
 def multiply_by_10(x):
+    """Multiply a number by 10
+
+    Parameters
+    ----------
+    x : float
+        value to multiply
+
+    Returns
+    -------
+    y : float
+        multiplied value
+    """
     return 10 * x
